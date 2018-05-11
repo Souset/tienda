0815b71685a783142cc4d0038532956a3acd2b29
César <cesar64091@gmail.com>
2018-05-11 18:09:12 +0200

    ooooeeeee

--- pagina_producto.php
+++ pagina_producto.php
@@ -160,7 +160,7 @@ efecto deseado
                                 </div>
                             </div>
                         </div>
-                <!-- FIN IMAGENES holaaaaaaaa-->
+                <!-- FIN IMAGENES holaaaaaaaaooooeeeeee-->
                 
                 <!-- INICIO TÍTULO, PRECIO, TABLA... -->
                         <div class="col-md-6">
