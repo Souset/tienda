c5b14e8bdc72c1fe04e72ce32e12638a87ce4dad
Joel <sousetpunset@gmail.com>
2018-05-11 17:57:41 +0200

    cvbsghser

--- pagina_producto.php
+++ pagina_producto.php
@@ -160,7 +160,7 @@ efecto deseado
                                 </div>
                             </div>
                         </div>
-                <!-- FIN IMAGENES -->
+                <!-- FIN IMAGENES holaaaaaaaa-->
                 
                 <!-- INICIO TÍTULO, PRECIO, TABLA... -->
                         <div class="col-md-6">
